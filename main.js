3adc449c18bb1a236a802a0edf9e7265c10db826
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-05-11 11:03:25 +0200

    prueba conexion recoger escudería

--- main.js
+++ main.js
@@ -6,15 +6,38 @@ function main() {
         "url": "http://ergast.com/api/f1/2022/drivers.json"
     };
 
+    var settingsConstructors = {
+        "async": true,
+        "type": "GET",
+        "url": "http://ergast.com/api/f1/2022/constructors.json"
+    };
+    var responseDrivers
+    var responseConstructor
 
     $.ajax(settingsDrivers).done(function (response) {
-        console.log(response);
-        responseDrivers = response;
 
-        for (let i = 0; i < response.MRData.DriverTable.Drivers.length; i++) {
+        console.log(response);
+        responseDrivers = response.MRData.DriverTable.Drivers;
+        console.log(responseDrivers)
+
+
+        for (let i = 0; i < responseDrivers.length; i++) {
             var piloto = document.createElement("div")
-            piloto.innerHTML = response.MRData.DriverTable.Drivers[i].givenName + " " + response.MRData.DriverTable.Drivers[i].familyName
+            piloto.innerHTML = responseDrivers[i].givenName + " " + responseDrivers[i].familyName
             document.getElementsByTagName("body")[0].appendChild(piloto)
+
+        }
+
+    });
+
+    $.ajax(settingsConstructors).done(function (response) {
+        console.log(response);
+        responseConstructor = response.MRData.ConstructorTable.Constructors;
+
+        for (let i = 0; i < responseConstructor.length; i++) {
+            var escuderia = document.createElement("div")
+            escuderia.innerHTML = responseConstructor[i].name
+            document.getElementsByTagName("body")[0].appendChild(escuderia)
 
         }
 
