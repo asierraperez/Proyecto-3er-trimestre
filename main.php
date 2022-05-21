d2a6f3cf4ed2b112a8e7381ddf794868062705a1
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-05-21 20:22:15 +0200

    Prueba insertar datos y seleccionarlos

--- main.php
+++ main.php
@@ -10,11 +10,33 @@
     }
     echo "Conectado correctamente <br>";
 
-    $consult = "CREATe DATABASE basedepruebas";
+    /*$consult = "CREATe DATABASE basedepruebas";
     if($connection->query($consult)==TRUE){
         echo "Base de datos creada correctamente";
     } else{
         echo "Error al crear la base de datos: ".$connection->error;
+    }*/
+
+    /*$consult = "Insert into escuderia values('ALP','Alpine',0)";
+    if($connection->query($consult)==TRUE){
+        echo "insercion correcta";
+    } else {
+        echo "fallo en insercion";
+    }*/
+
+    $selection = "select * from escuderia";
+    $result = $connection->query($selection);
+
+    if($result->num_rows>0){
+        echo "Exito en select <br>";
+        echo "Código | Nombre | Puntos <br>";
+        while($row=$result->fetch_assoc()){
+            echo $row["ID"]." | " . $row["nombre"]." | " . $row["puntos"] . "<br>"; 
+        }
+
+
+    } else {
+        echo "Error en select";
     }
 
 ?>
